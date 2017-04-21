e3d8d6ccda19c19dd9f191d4b4f931db938ee813
StefanKuppelwieser <edelblistar@online.de>
2017-04-21 22:57:53 +0200

    Update shell-files
    Translate german comments to english

--- run_AverageSoldProductsPerYear.sh
+++ run_AverageSoldProductsPerYear.sh
@@ -1,32 +1,32 @@
 hadoop fs -rm -r output
 
 echo ""
-echo "#############################"
-echo "## Output wurde geloescht! ##"
-echo "#############################"
+echo "########################"
+echo "## Output was deleted ##"
+echo "########################"
 echo ""
 
 hadoop jar SportBrands-Java/averageSoldProductsPerYear.jar AverageSoldProductsPerYear /user/student_63/input1 /user/student_63/output
 
 echo ""
-echo "#############################"
-echo "### WordCount lief durch ####"
-echo "#############################"
+echo "######################"
+echo "### Java run done ####"
+echo "#######################"
 echo ""
 
 
 hdfs dfs -ls /user/student_63/output
 
 echo ""
-echo "#######################################"
-echo "### Zeige Inhalt vom Output Ordner ####"
-echo "########################################"
+echo "################################################"
+echo "### Show the content of the outpult folder ####"
+echo "###############################################"
 echo ""
 
 hdfs dfs -cat /user/student_63/output/part-00000
 
 echo ""
 echo "#######################"
-echo "### Zeige Ergebnis ####"
+echo "### Show the results ####"
 echo "#######################"
 echo ""
